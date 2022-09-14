19e6d099da9e3f83474ca8403b859a3676a27c48
angelika22 <70664491+angelika22@users.noreply.github.com>
2022-09-14 21:51:43 +0300

    Add files via upload

---
+++ prob 5.py
@@ -0,0 +1,19 @@
+def ad():
+    a=int(input("a- "))
+    b=int(input("b- "))
+    c=int(input("c- "))
+    d=int(input("d- "))
+    s=(a*d+b*c)/(c*d)
+    return(s)
+
+print(ad())
+
+def p():
+    a=int(input("a- "))
+    b=int(input("b- "))
+    c=int(input("c- "))
+    d=int(input("d- "))
+    p=(a*c)/(b+d)
+    returnn(p)
+
+print(p())
